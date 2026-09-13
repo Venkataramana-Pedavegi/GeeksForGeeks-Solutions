5d6cc18fc9e62474b7502aff95b9374de29e5d0c
VENKATA RAMANA PEDAVEGI <venkataramanapedavegi9@gmail.com>
2026-09-13 17:47:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Any Common Word in Two Lists/any-common-word-in-two-lists.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean commonString(String[] s, String[] t) {
+        // code here
+        HashSet<String> set = new HashSet<>();
+
+               for (String str : s) {
+                   set.add(str);
+               }
+
+               for (String str : t) {
+                   if (set.contains(str)) {
+                       return true;
+                   }
+               }
+
+               return false;
+    }
+}
