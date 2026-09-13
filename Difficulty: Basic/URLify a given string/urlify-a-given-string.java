9d75226fa0473b377868198d3fb8413c29dfa9a0
VENKATA RAMANA PEDAVEGI <venkataramanapedavegi9@gmail.com>
2026-09-13 17:43:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/URLify a given string/urlify-a-given-string.java
+++ Difficulty: Basic/URLify a given string/urlify-a-given-string.java
@@ -1,18 +1,20 @@
 class Solution {
     static String URLify(String s) {
         // code here
-       StringBuilder ans = new StringBuilder();
+        // code here
+               StringBuilder ans = new StringBuilder();
 
-              for (int i = 0; i < s.length(); i++) {
-                  char ch = s.charAt(i);
+               for (int i = 0; i < s.length(); i++) {
+                   char ch = s.charAt(i);
 
-                  if (ch == ' ') {
-                      ans.append("%20");
-                  } else {
-                      ans.append(ch);
-                  }
-              }
+                   if (ch == ' ') {
+                       ans.append("%20");
+                   } else {
+                       ans.append(ch);
+                   }
+               }
 
-              return ans.toString(); 
+               return ans.toString();
+        
     }
 }
