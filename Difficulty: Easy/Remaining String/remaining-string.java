e13d9bb1eed4ed2275cd8176f09f2c07b2e63ccd
VENKATA RAMANA PEDAVEGI <venkataramanapedavegi9@gmail.com>
2026-09-13 11:20:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remaining String/remaining-string.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String printString(String s, char ch, int count) {
+        int occurrences = 0;
+
+                for (int i = 0; i < s.length(); i++) {
+                    if (s.charAt(i) == ch) {
+                        occurrences++;
+
+                        if (occurrences == count) {
+                            return s.substring(i + 1);
+                        }
+                    }
+                }
+
+                return "";
+        
+    }
+}
