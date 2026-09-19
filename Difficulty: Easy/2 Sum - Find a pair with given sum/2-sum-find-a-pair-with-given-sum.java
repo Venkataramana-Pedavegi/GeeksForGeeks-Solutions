03101f54e89f5ee81e5ef3272928af3f37733588
VENKATA RAMANA PEDAVEGI <venkataramanapedavegi9@gmail.com>
2026-09-19 20:52:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/2 Sum - Find a pair with given sum/2-sum-find-a-pair-with-given-sum.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> twoSum(int arr[], int target) {
+
+        List<Integer> ans = new ArrayList<>();
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : arr) {
+
+            int needed = target - num;
+
+            if (set.contains(needed)) {
+                ans.add(needed);
+                ans.add(num);
+                return ans;
+            }
+
+            set.add(num);
+        }
+
+        return ans;
+    }
+}
