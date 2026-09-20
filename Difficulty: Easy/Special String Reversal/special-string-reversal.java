bbcf7672b5134f7c3d6d3259e1468d37c2a72e1a
VENKATA RAMANA PEDAVEGI <venkataramanapedavegi9@gmail.com>
2026-09-20 19:24:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Special String Reversal/special-string-reversal.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String reverseString(String s) {
+        char[] arr = s.toCharArray();
+
+                int left = 0;
+                int right = arr.length - 1;
+
+                while (left < right) {
+
+                    while (left < right && !Character.isLetter(arr[left])) {
+                        left++;
+                    }
+
+                    while (left < right && !Character.isLetter(arr[right])) {
+                        right--;
+                    }
+
+                    char temp = arr[left];
+                    arr[left] = arr[right];
+                    arr[right] = temp;
+
+                    left++;
+                    right--;
+                }
+
+                return new String(arr);
+        
+
+    }
+}
