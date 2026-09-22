0c17420024ca922794d261b001cc6b90cbffd627
VENKATA RAMANA PEDAVEGI <venkataramanapedavegi9@gmail.com>
2026-09-22 20:44:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Three Sum/three-sum.java
@@ -0,0 +1,54 @@
+class Solution {
+    public static ArrayList<ArrayList<Integer>> triplets(int[] arr) {
+
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length - 2; i++) {
+
+            // Skip duplicate first elements
+            if (i > 0 && arr[i] == arr[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = arr.length - 1;
+
+            while (left < right) {
+
+                int sum = arr[i] + arr[left] + arr[right];
+
+                if (sum == 0) {
+
+                    ArrayList<Integer> temp = new ArrayList<>();
+                    temp.add(arr[i]);
+                    temp.add(arr[left]);
+                    temp.add(arr[right]);
+
+                    result.add(temp);
+
+                    // Skip duplicate left values
+                    while (left < right && arr[left] == arr[left + 1]) {
+                        left++;
+                    }
+
+                    // Skip duplicate right values
+                    while (left < right && arr[right] == arr[right - 1]) {
+                        right--;
+                    }
+
+                    left++;
+                    right--;
+
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
+}
